61955e584a52510114a28f97b1abde251b3ed2b4
hajainakazz <116579259+hajainakazz@users.noreply.github.com>
2025-04-02 11:41:35 +0200

    Add files via upload

---
+++ projet_devWeb_V5/projet_devWeb/src/main/java/com/example/projet_devWeb/controller/PageDeGardeController.java
@@ -0,0 +1,44 @@
+package com.example.projet_devWeb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PageDeGardeController {
+
+    // Méthode pour afficher la page d'accueil de la bibliothèque connectée
+    @GetMapping("/accueil")
+    public String afficherPageGarde() {
+        return "PageGarde";  // Nom du template Thymeleaf, ici "PageGarde.html"
+    }
+
+    // Rediriger vers la page de connexion
+    @GetMapping("/login")
+    public String afficherPageConnexion() {
+        return "connexion";  // Nom du template pour la page de connexion
+    }
+
+    // Rediriger vers la page d'inscription
+    @GetMapping("/signup")
+    public String afficherPageInscription() {
+        return "inscription";  // Nom du template pour la page d'inscription
+    }
+
+    // Rediriger vers la page agenda
+    @GetMapping("/agenda")
+    public String afficherPageAgenda() {
+        return "agenda";  // Nom du template pour la page agenda
+    }
+
+    // Rediriger vers la page de contact
+    @GetMapping("/contact")
+    public String afficherPageContact() {
+        return "contact";  // Nom du template pour la page de contact
+    }
+
+    // Rediriger vers la page itinéraire
+    @GetMapping("/itineraire")
+    public String afficherPageItineraire() {
+        return "itineraire";  // Nom du template pour la page itinéraire
+    }
+}
