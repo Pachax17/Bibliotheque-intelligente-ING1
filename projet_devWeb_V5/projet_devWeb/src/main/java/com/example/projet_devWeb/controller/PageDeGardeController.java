320c9997b115b4be809e8243e2fc43fe515ac4a9
Clement R <104372704+IDEFIX95@users.noreply.github.com>
2025-04-04 12:09:34 +0200

    Update PageDeGardeController.java
    Mis à jour de la page pour rajouter objet, administrateur et accueil

--- projet_devWeb_V5/projet_devWeb/src/main/java/com/example/projet_devWeb/controller/PageDeGardeController.java
+++ projet_devWeb_V5/projet_devWeb/src/main/java/com/example/projet_devWeb/controller/PageDeGardeController.java
@@ -1,27 +1,51 @@
 package com.example.projet_devWeb.controller;
 
+import com.example.projet_devWeb.model.Utilisateur;
+
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PageDeGardeController {
 
     // Méthode pour afficher la page d'accueil de la bibliothèque connectée
+    @GetMapping("/")
+    public String redirigerVersAccueil() {
+        return "redirect:/accueil";
+    }
+
     @GetMapping("/accueil")
-    public String afficherPageGarde() {
-        return "PageGarde";  // Nom du template Thymeleaf, ici "PageGarde.html"
+    public String afficherAccueil(Model model, Principal principal) {
+        model.addAttribute("username", principal != null ? principal.getName() : null);
+        return "accueil";
     }
 
     // Rediriger vers la page de connexion
-    @GetMapping("/login")
-    public String afficherPageConnexion() {
-        return "connexion";  // Nom du template pour la page de connexion
+    @GetMapping("/connexion")
+    public String afficherFormulaireConnexion() {
+        return "connexion"; // connexion.html
     }
 
     // Rediriger vers la page d'inscription
-    @GetMapping("/signup")
-    public String afficherPageInscription() {
-        return "inscription";  // Nom du template pour la page d'inscription
+    @GetMapping("/inscription")
+    public String afficherFormulaireInscription(Model model) {
+        model.addAttribute("utilisateur", new Utilisateur());
+        return "inscription"; // inscription.html
+    }
+
+    // Rediriger vers la page profil
+    @GetMapping("/profil")
+    public String afficherProfil() {
+        return "profil"; // profil.html
+    }
+    
+    // Rediriger vers la page administrateur
+    @GetMapping("/administrateur")
+    public String afficherUtilisateur(Model model) {
+        return "administrateur";
     }
 
     // Rediriger vers la page agenda
