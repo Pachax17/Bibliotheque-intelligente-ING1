797f034da0d613665f0658b31624ffa0691c72a1
Clement R <104372704+IDEFIX95@users.noreply.github.com>
2025-04-06 15:52:00 +0200

    Add files via upload

---
+++ projet_devWeb_V5/projet_devWeb/src/main/java/com/example/projet_devWeb/repository/DemandeAjoutObjetRepository.java
@@ -0,0 +1,10 @@
+package com.example.projet_devWeb.repository;
+
+import com.example.projet_devWeb.model.DemandeAjoutObjet;
+import com.example.projet_devWeb.model.StatutDemande;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface DemandeAjoutObjetRepository extends JpaRepository<DemandeAjoutObjet, Long> {
+    List<DemandeAjoutObjet> findByStatus(StatutDemande status);
+}
