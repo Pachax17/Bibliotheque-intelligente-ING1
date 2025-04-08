5731e2b3edd8660b5932165ac68dd1bf07e8a0b3
Pachax17 <131616395+Pachax17@users.noreply.github.com>
2025-04-08 22:42:18 +0200

    Delete projet_devWeb_V8 directory

--- projet_devWeb_V8/projet_devWeb/src/main/java/com/example/projet_devWeb/config/CompteNonVerifieException.java
+++
@@ -1,9 +0,0 @@
-package com.example.projet_devWeb.config;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class CompteNonVerifieException extends AuthenticationException {
-    public CompteNonVerifieException(String msg) {
-        super(msg);
-    }
-}
--- projet_devWeb_V8/projet_devWeb/src/main/java/com/example/projet_devWeb/model/StatutDemande.java
+++
@@ -1,7 +0,0 @@
-package com.example.projet_devWeb.model;
-
-public enum StatutDemande {
-    EN_ATTENTE,
-    ACCEPTEE,
-    REFUSEE
-}
